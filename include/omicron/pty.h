6c3d1a7fec2e26957d162d169ea005a5e4268bee
levtcode <levtcode@gmail.com>
2026-08-18 18:09:49 +0200

    refactor: replaced header guards with #pragma once

--- include/omicron/pty.h
+++ include/omicron/pty.h
@@ -1,13 +1,9 @@
 /* PTY: crea el par master/slave, lanza el hijo ya aislado y bombea E/S. */
-#ifndef OMICRON_PTY_H
-#define OMICRON_PTY_H
+#pragma  once
 
 #include <sys/types.h>
 
-#ifndef OM_POLICY_FWD
-#define OM_POLICY_FWD
 typedef struct om_policy om_policy;
-#endif
 
 /* Códigos de salida reservados del hijo: distinguen "no se ejecutó" de "falló". */
 #define OM_EXIT_SANDBOX 127 /* el sandbox no se pudo aplicar; nunca hubo exec */
@@ -27,5 +23,3 @@ int om_pty_pump(om_pty *t);
 int  om_pty_resize(om_pty *t, unsigned rows, unsigned cols);
 int  om_pty_wait(om_pty *t, int *status);
 void om_pty_kill(om_pty *t);
-
-#endif /* OMICRON_PTY_H */
